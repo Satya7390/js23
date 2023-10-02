5f46110f53e3ea4ee6d077a3ddbff41cbbfd1e29
satya <satyanandgupta845@gmail.com>
2023-10-02 07:56:41 +0530

    first commit

---
+++ button.js
@@ -0,0 +1,56 @@
+// a=5.2
+// b=10
+// document.getElementById('satya').innerHTML=(a+b)
+// document.write(a+b)
+// window.alert("Hii Satya")
+// console.log(a+b)
+//document.write(button)
+//window.alert( onclick="document.getElementById('satya').innerHTML")
+// let a=10
+// let b= 12
+// const c= a+b
+//document.getElementById('satya').innerHTML=(c)
+// Or//
+// document.write(c)
+// var a=10.12345;
+// var b=20;
+// var c= a+b;
+// window.alert()
+// function satya(a,b){
+//     return (a*b);
+// }
+// let x = satya( 12,10);
+// document.write(x);
+// const satya = {
+//     firstname:'Satya',lastname:'Gupta',qualification:'Student'
+// };
+// document.getElementById('satya').innerHTML=satya.firstname+ " is a "+ satya.qualification + " and last name is "+ satya.lastname; // OR
+// document.write( satya.firstname +" is a " + satya.qualification)
+// const person = {
+//     firstname:'Satya',
+//     lastname:'Gupta',
+//     fullname:
+//     function(){
+//         return this.firstname + " " + this.lastname;
+    
+//     }
+// }
+// const user = {
+//     firstname:'Nityanand',
+//     lastname:'Gupta'
+//     // qualification : 'Mechanical Engineer'
+//    }
+// let fullname = person.fullname.bind(person);
+// //let fullname = person.fullname.bind(user);
+// document.getElementById('satya').innerHTML = fullname();
+const name =['Ram',"Shyam", "Mohan"];
+let size=name.length;
+ document.getElementById('satya').innerHTML=name;
+ document.getElementById('satya').innerHTML=name[2];
+//  function satya(){
+//     name.push("Gupta");
+//  }
+//  document.getElementById('satya').innerHTML=name;
+
+
+
